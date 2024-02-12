b9f79181849068be6545a52350b904f75488dbd8
Ezechiel AGBAN <ezechiel9087@gmail.com>
2024-02-12 18:27:28 +0000

    setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen ">
+      <p className="text-center text-5xl text-red-600 font-extrabold">
+        Sang pour Sang
+      </p>
+    </div>
+  );
+}
+
+export default App;
